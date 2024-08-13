862a4e1bae4f8dea10c7880a1829d89af4b3d8a6
PopovaOlha <fc.odessa.10@gmail.com>
2024-08-13 14:31:10 +0300

    feat: add dependencies for the project

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      'react-refresh',
+      'react',
+      'react-hooks',
+      'react-compiler',
+      'prettier'
+    ],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react-compiler/react-compiler': 'error',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
